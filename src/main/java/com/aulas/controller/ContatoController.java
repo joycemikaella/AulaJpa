079971be7d94207335b9a6f7489fa7e7d62cd925
Joyce Mikaella <joycemikaella@gmail.com>
2022-08-15 18:54:31 -0300

    atualizacao github

---
+++ src/main/java/com/aulas/controller/ContatoController.java
@@ -0,0 +1,62 @@
+package com.aulas.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.aulas.entidades.Contato;
+import com.aulas.repository.ContatoRepository;
+
+@Controller
+@RequestMapping("/")
+
+public class ContatoController {
+	
+	@Autowired
+	private ContatoRepository repo;	
+
+	@GetMapping
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/cadastro")
+	public String formCadastro() {
+		return "cadastro";
+	}
+
+	@GetMapping("/consulta")
+	public String formConsulta(Model model) {
+		Iterable<Contato> contatos = repo.findAll();
+		model.addAttribute("contatos", contatos);
+		return "consulta";
+	}
+	
+	@GetMapping("/consulta/{idcontato}")
+	public String formEditar(@PathVariable ("idcontato") int id, Model model) {
+		Contato contato = repo.findById(id).get();
+		model.addAttribute("contato", contato);
+		return "edicao";
+	}
+	
+	@PostMapping("/cadastro")
+	public String salvar (Contato contato) {
+		//contato.setId(contatos.size()+1);
+		//System.out.printf("Nome:%s, \nFone:%s, \nEmail:%s",
+		//	contato.getNome(), contato.getFone(), contato.getEmail());
+		//contatos.add(contato);
+		repo.save(contato);
+		return "redirect:/consulta";
+	}
+	
+	
+	@GetMapping("/delete/{idcontato}")
+	public String delete(@PathVariable ("idcontato") int id) {
+		repo.deleteById(id);
+		return "redirect:/consulta";
+	}
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into contato(nome, fone, email) values('joyce', '886244', 'joyce@mail.com');
+insert into contato(nome, fone, email) values('mikaella', '343334', 'mika@mail.com');
+insert into contato(nome, fone, email) values('hugo', '887686', 'hugo@mail.com');
+
+insert into Casa(descricao, dimensao, comodo) values('abridor', 'MIUDEZAS', 'Cozinha');
+insert into Casa(descricao, dimensao, comodo) values('Assento sanitario', 'MOBILIA', 'Utilidades');
+insert into Casa(descricao, dimensao, comodo) values('Torneira', 'MIUDEZAS', 'Necessidades');
